30bc030d8f48a5b9645b2bce018c2e0c72817d80
dinesh punni <dinesh.punni@hotmail.de>
2016-06-25 15:04:34 +0200

    test

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance=this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    public void test()
+    {
+        Debug.Log("buh");
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag== "Enemy")
+        {
+            LevelManager.instance.LoadLevel("MainMenu");
+        }
+    }
+
+}
