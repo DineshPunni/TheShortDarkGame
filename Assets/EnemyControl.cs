f3b416da9e7ea0f9a7d428df17975a7415a3d5a3
dinesh punni <dinesh.punni@hotmail.de>
2016-06-25 14:27:26 +0200

    testCommit

---
+++ Assets/EnemyControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+
+    int maxXPos = 20;
+    int maxZPos = 20;
+
+	void Start () {
+        InvokeRepeating("RandomSpawn",1,5);
+	}
+	
+	void Update () {
+	
+	}
+
+    public void RandomSpawn()
+    {
+        int xPos = Random.Range(maxXPos*-1, maxXPos);
+        int zPos = Random.Range(maxZPos*-1, maxZPos);
+
+        gameObject.transform.position = new Vector3(xPos, 0, zPos);
+    }
+
+}
