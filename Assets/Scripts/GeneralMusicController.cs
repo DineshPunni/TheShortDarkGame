8b0283ec4cb434f3e160d473c7939b6bf8a21785
jpeinuni <jpein.uni@gmail.com>
2016-06-25 15:34:54 +0200

    added background music

---
+++ Assets/Scripts/GeneralMusicController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralMusicController : MonoBehaviour {
+
+    public AudioClip backgroundSound;
+    public AudioSource source;
+    private float volStart = .5f;
+    private float volMax = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    void Awake ()
+    {
+        source = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
