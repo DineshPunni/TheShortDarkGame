ac03aecedb305d2383171b86ca547bcbe0ff69a1
dinesh punni <dinesh.punni@hotmail.de>
2016-06-25 16:01:03 +0200

    thunder added

---
+++ Assets/ThunderControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThunderControl : MonoBehaviour {
+
+    public float thunderTimer = 1;
+    public GameObject thunder;
+
+	void Start () {
+
+        InvokeRepeating("PlayThunder", 1, 5);
+	}
+	
+	void Update () {
+	
+	}
+
+    void PlayThunder()
+    {
+        StartCoroutine(ThunderRoutine());
+    }
+
+    IEnumerator ThunderRoutine()
+    {
+        thunder.SetActive(true);
+
+        while (thunderTimer > 0)
+        {
+            thunderTimer -= Time.deltaTime;
+            yield return null;
+        }
+        thunder.SetActive(false);
+        thunderTimer = 0.5fa;
+    }
+}
